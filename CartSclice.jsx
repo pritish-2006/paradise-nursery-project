3ebef188ee7a168bb41c55847758af02ea576d11
Pritiranjan Behera <pritiranjanbehera016@gmail.com>
2026-05-09 23:58:52 +0530

    Create cartSlice for managing cart state

---
+++ CartSclice.jsx
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  items: []
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+
+  reducers: {
+    addItem: (state, action) => {
+      state.items.push(action.payload);
+    },
+
+    removeItem: (state, action) => {
+      state.items = state.items.filter(
+        (item) => item.id !== action.payload
+      );
+    },
+
+    updateQuantity: (state, action) => {
+      const item = state.items.find(
+        (item) => item.id === action.payload.id
+      );
+
+      if (item) {
+        item.quantity = action.payload.quantity;
+      }
+    }
+  }
+});
+
+export const {
+  addItem,
+  removeItem,
+  updateQuantity
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
