d635234e53ab7a1f2ccdd5430aa7e078769dc2c0
Pritiranjan Behera <pritiranjanbehera016@gmail.com>
2026-05-09 23:57:33 +0530

    Create initial App component with basic layout

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", paddingTop: "100px" }}>
+      <h1>Paradise Nursery</h1>
+
+      <p>Beautiful plants for your home and garden.</p>
+
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
