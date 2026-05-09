8a067f787b74fec3ba77af586d4a5b9284b5c569
Pritiranjan Behera <pritiranjanbehera016@gmail.com>
2026-05-09 23:46:38 +0530

    Add AboutUs component for nursery information

---
+++ AboutUs.jsx
@@ -0,0 +1,13 @@
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Paradise Nursery</h1>
+
+      <p>
+        Paradise Nursery provides beautiful indoor and outdoor plants.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
